0fae7b907cef2494e74d0661cea18664bf41ff60
Diem Nguyen <diiemnguyen@yahoo.com>
2013-10-28 15:21:04 -0700

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -1,7 +1,7 @@
 <div id="framework-footer" style="font-size:11.5px; margin-top:5px;">
 	<div style="line-height:0.92em; padding-top:5px; padding-bottom:5px; border-bottom:#ccc 1px solid; margin-bottom:5px;"></div>
-	<div id="footer-nav"><a href="http://www.cisco.com/web/siteassets/contacts/" accesskey="9" target="_blank">Contacts &amp; Feedback</a> | <a href="http://www.cisco.com/web/help/" accesskey="0" target="_blank">Help</a> | <a href="http://www.cisco.com/web/siteassets/sitemap/index.html" target="_blank">Site Map</a></div>
+	div id="footer-nav"><a href="" accesskey="9" target="_blank">Contacts &amp; Feedback</a></div>
 
-    <div id="footer-copyright">&reg; 1992-2007 Cisco Systems Inc. All rights reserved.</div><div id="footer-legal"><a href="http://www.cisco.com/web/siteassets/legal/terms_condition.html" target="_blank">Terms &amp; Conditions</a> | <a href="http://www.cisco.com/web/siteassets/legal/privacy.html" target="_blank">Privacy Statement</a> | <a href="http://www.cisco.com/web/siteassets/legal/privacy.html#cookies" target="_blank">Cookie Policy</a> | <a href="http://www.cisco.com/web/siteassets/legal/trademark.html" target="_blank">Trademarks of Cisco Systems Inc.</a></div>
+    	div id="footer-copyright">CopyRight 2013</div><div id="footer-legal"></div>
 </div>
 
